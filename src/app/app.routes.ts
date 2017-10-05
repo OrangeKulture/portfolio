a5b97105baed846fc8c63dcdc4ad2a01cad6acea
Jorge A <ardonz@gmail.com>
2017-10-05 15:28:08 -0600

    minor changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import {HomeComponent} from './components/home/home.component';
 import {AboutComponent} from './components/about/about.component';
 import {ContactComponent} from './components/contact/contact.component';
 import {PortfolioComponent} from './components/portfolio/portfolio.component';
-import {ProjectDetailsComponent} from './components/project-details/project-details.component';
 
 
 export const appRoutes: Routes = [
@@ -11,5 +10,4 @@ export const appRoutes: Routes = [
     {path: 'about', component: AboutComponent},
     {path: 'portfolio', component: PortfolioComponent},
     {path: 'contact', component: ContactComponent},
-    {path: 'project/:name', component: ProjectDetailsComponent}
 ];
